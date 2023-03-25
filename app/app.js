b97b3928a172aa64bd1568a1195ef57bd3ee3def
Samara Neves <sneves0@hotmail.com>
2023-03-25 17:00:51 -0300

    feat: inclusao de copiar codigo da cor para area de transferencia

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import { elementCard } from './utils/element.js'
+import { copyToClipboard, getKeyPressedCode } from './utils/keyboard.js'
 import { Palette } from './utils/palette.js'
 
 const paletteArea = document.querySelector('.generator__section')
+
 
 const pallette = new Palette()
 
@@ -9,16 +11,50 @@ const colors = pallette.createPalette()
 
 const elements = colors.map(item => elementCard(item)).join('')
 
-window.onload = paletteArea.innerHTML = elements;
-
-document
-    .querySelector('#generator__palette__button')
-    .addEventListener('click', () => {
-        const cardPalette = document.querySelectorAll('.card__background')
-
-            if(cardPalette) {
-                cardPalette.forEach(element => {
-                    element.style.backgroundColor = pallette.newColor()
+const generateNewPallette = (elements) => {
+            if(elements) {
+                elements.forEach(element => {
+                    const backgroundElement = element.firstElementChild
+                    const textHexadecimalElement = element.lastElementChild
+                    const newColor = pallette.newColor()
+                
+                    backgroundElement.style.backgroundColor = newColor
+                    textHexadecimalElement.textContent = newColor
                 });
-            }  
-})
+            } 
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    paletteArea.innerHTML = elements;
+
+    const cards = document.querySelectorAll('.card')
+
+    document
+    .querySelector('#generator__palette__button')
+    .addEventListener('click', () => generateNewPallette(cards))
+
+    cards.forEach((element) => {
+        element.addEventListener('click', () => {
+            const textElement = element.lastElementChild
+            copyToClipboard(textElement)
+        })
+    });
+
+    document.addEventListener('keypress', (event) => {
+        const keyPressed = getKeyPressedCode(event)
+        if(keyPressed === 32) {
+            generateNewPallette(cards)
+        }
+
+        if(keyPressed === 99) {
+            const teste = document.querySelectorAll('.card__hexadecimal')
+            
+            const text = Array.from(teste, (item) => item.innerText)
+            console.log(text)
+        }
+        
+    })
+});
+
+
+
--- app/utils/keyboard.js
+++ app/utils/keyboard.js
@@ -1,6 +1,21 @@
-export class Keyboard {
-    constructor(event) {
-        this.keyboardEvent = event
-    }
+const createRangeSelection = () => document.createRange()
 
+const selectNode = (range, element) => range.selectNode(element)
+
+const removeAlllSelectedRanges = () => window.getSelection().removeAllRanges()
+
+const addRangeSelection = (range) => window.getSelection().addRange(range)
+
+export const getKeyPressedCode = (event) => event.keyCode || event.which
+
+export const copyToClipboard = (element) => {
+    const range = createRangeSelection()
+    selectNode(range, element)
+
+    removeAlllSelectedRanges()
+    addRangeSelection(range)
+
+    document.execCommand("copy")
+    alert('copiado')
+    removeAlllSelectedRanges()
 }
