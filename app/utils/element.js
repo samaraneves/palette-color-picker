07bb7706af87e6131ffa0648da5c98da62aa72f4
Samara Neves <sneves0@hotmail.com>
2024-02-07 13:12:54 -0300

    fix: correcao na obtencao de dado textual em elementos html do tipo paragraph

--- app/utils/element.js
+++ app/utils/element.js
@@ -1,35 +1,39 @@
-export const createHTMLelement = (tagName = '', className = '', textContent = '') => {
-    const element = document.createElement(tagName);
-    element.className = className
+export const createHTMLelement = (
+  tagName = "",
+  className = "",
+  textContent = ""
+) => {
+  const element = document.createElement(tagName);
+  element.className = className;
 
-    if(textContent) {
-        const textContentElement = document.createTextNode(textContent)
-        element.appendChild(textContentElement)
-    }
-    return element
-}
+  if (textContent) {
+    const textContentElement = document.createTextNode(textContent);
+    element.appendChild(textContentElement);
+  }
+  return element;
+};
 
-export const toast = (message = '', timeout = 8100) => {
-    const element = createHTMLelement(
-        'strong',
-        'generator__alert', 
-        message
-    )
+export const toast = (message = "", timeout = 8100) => {
+  const element = createHTMLelement("strong", "generator__alert", message);
 
-    document.body.insertBefore(element, document.querySelector('.generator__title'))
+  document.body.insertBefore(
+    element,
+    document.querySelector(".generator__title")
+  );
 
-    setTimeout(() => {
-        element.remove()
-    }, timeout);
-}
+  setTimeout(() => {
+    element.remove();
+  }, timeout);
+};
 
-export const selectFirstElementChild = (element) => element.firstElementChild
+export const selectFirstElementChild = (element) => element.firstElementChild;
 
-export const selectLastElementChild = (element) => element.lastElementChild
+export const selectLastElementChild = (element) => element.lastElementChild;
 
-export const styleHTMLElement = (element, cssProperty, cssPropertyValue) => element.style[cssProperty] = cssPropertyValue
+export const styleHTMLElement = (element, cssProperty, cssPropertyValue) =>
+  (element.style[cssProperty] = cssPropertyValue);
 
-export const getTextContentHTMLElement = (element) => element.textContent
+export const getTextContentHTMLElement = (element) => element.innerText;
 
-export const changeTextContentHTMLElement = (element, newContent) => element.textContent = newContent
-
+export const changeTextContentHTMLElement = (element, newContent) =>
+  (element.textContent = newContent);
