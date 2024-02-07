01c54d42420095ee4ac40f08b011e0b02353f40b
Samara Neves <sneves0@hotmail.com>
2024-02-07 13:23:50 -0300

    fix: correcao da obtencao da string da cor selecionada para area de transferencia

--- app/app.js
+++ app/app.js
@@ -1,69 +1,76 @@
-import { 
-    changeTextContentHTMLElement, 
-    getTextContentHTMLElement, 
-    selectLastElementChild, 
-    styleHTMLElement, 
-    toast
-} from './utils/element.js'
-import { copyStringToClipboard, handleKeyPress } from './utils/keyboard.js'
-import { convertToArray, pipe } from './utils/operators.js'
-import { newColor } from './utils/palette.js'
+import {
+  changeTextContentHTMLElement,
+  getTextContentHTMLElement,
+  selectLastElementChild,
+  styleHTMLElement,
+  toast,
+} from "./utils/element.js";
+import { copyStringToClipboard, handleKeyPress } from "./utils/keyboard.js";
+import { convertToArray, pipe } from "./utils/operators.js";
+import { newColor } from "./utils/palette.js";
 
 const generateNewPalette = (elements) => {
-            if(elements) {
-                elements.forEach(element => {
-                    const backgroundElement = element.firstElementChild
-                    const textHexadecimalElement = element.lastElementChild
-                    const color = newColor()
-                
-                    styleHTMLElement(backgroundElement, 'backgroundColor', color)
-                    changeTextContentHTMLElement(textHexadecimalElement, color)
-                });
-            } 
-}
+  if (elements) {
+    elements.forEach((element) => {
+      const backgroundElement = element.firstElementChild;
+      const textHexadecimalElement = element.lastElementChild;
+      const color = newColor();
+
+      styleHTMLElement(backgroundElement, "backgroundColor", color);
+      changeTextContentHTMLElement(textHexadecimalElement, color);
+    });
+  }
+};
 
 const onClickCard = (element) => {
+  const textElement = pipe(selectLastElementChild)(element);
 
-    const textElement = pipe(selectLastElementChild)(element)
+  const textElementString = pipe(getTextContentHTMLElement)(textElement);
 
-    const textElementString = pipe(getTextContentHTMLElement)(textElement)
+  toast(
+    `A cor ${textElementString} foi copiada para a sua área de transferência!`
+  );
 
-    toast(`A cor ${textElementString} foi copiada para a sua área de transferência!`)
-
-    copyStringToClipboard(textElement)
-}
+  copyStringToClipboard(textElementString);
+};
 
 const copyPalette = () => {
-    const cardHexadecimalElements = document.querySelectorAll('.card__hexadecimal')
-            
-    const arrayToContentElements = convertToArray(cardHexadecimalElements, item => getTextContentHTMLElement(item))
+  const cardHexadecimalElements =
+    document.querySelectorAll(".card__hexadecimal");
 
-    toast(`A paleta foi copiada para a sua área de transferência!`)
+  const arrayToContentElements = convertToArray(
+    cardHexadecimalElements,
+    (item) => getTextContentHTMLElement(item)
+  );
 
-    copyStringToClipboard(arrayToContentElements)
-}
+  toast(`A paleta foi copiada para a sua área de transferência!`);
+
+  copyStringToClipboard(arrayToContentElements);
+};
 
 const onDOMContentLoaded = () => {
-    const cards = Array.from(document.querySelectorAll('.card'))
+  const cards = Array.from(document.querySelectorAll(".card"));
 
-    const generatorPaletteButton = document.querySelector('#generator__palette__button')
-    
+  const generatorPaletteButton = document.querySelector(
+    "#generator__palette__button"
+  );
+
+  generateNewPalette(cards);
+
+  const keyFunctions = [
+    [32, () => generateNewPalette(cards)],
+    [99, copyPalette],
+  ];
+
+  generatorPaletteButton.addEventListener("click", () =>
     generateNewPalette(cards)
+  );
 
-    const keyFunctions = [
-        [32, () => generateNewPalette(cards)],
-        [99, copyPalette],
-    ];
+  cards.forEach((card) =>
+    card.addEventListener("click", () => onClickCard(card))
+  );
 
-    generatorPaletteButton.addEventListener('click', () => generateNewPalette(cards))
-
-    cards.forEach((card) => card.addEventListener('click', () => onClickCard(card)))
-
-    document.addEventListener('keypress', handleKeyPress(keyFunctions))
-
-}
+  document.addEventListener("keypress", handleKeyPress(keyFunctions));
+};
 
 window.addEventListener("DOMContentLoaded", onDOMContentLoaded);
-
-
-
--- app/utils/keyboard.js
+++ app/utils/keyboard.js
@@ -1,11 +1,12 @@
-export const getKeyPressedCode = (event) => event.keyCode || event.which
+export const getKeyPressedCode = (event) => event.keyCode || event.which;
 
-export const copyStringToClipboard = (string) => navigator.clipboard.writeText(string)
+export const copyStringToClipboard = (string) =>
+  navigator.clipboard.writeText(string);
 
 export const handleKeyPress = (keyFunctions) => (event) => {
-    const keyCode = getKeyPressedCode(event)
+  const keyCode = getKeyPressedCode(event);
 
-    const keyFunction = keyFunctions.find(([code]) => code === keyCode)?.[1];
+  const keyFunction = keyFunctions.find(([code]) => code === keyCode)?.[1];
 
-    if (keyFunction) keyFunction();
+  if (keyFunction) keyFunction();
 };
